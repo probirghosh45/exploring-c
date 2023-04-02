29b05375b6ca0cd85771d3d8860cf71940c7e382
probir ghosh <probirghosh@outlook.com>
2023-04-02 12:29:25 +0600

    input-output txt added

--- intial.cpp
+++ intial.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main(){
-    printf("Probir,Focus on the Journey\n");
-    printf("Welcome to Phitron Spring 2023,Batch 3"); 
+    // printf("Probir,Focus on the Journey\n");
+    // printf("Welcome to Phitron Spring 2023,Batch 3\n");
+    // printf("Learning C Language with Phitron"); 
+    
+    printf("Welcome to Phitron Spring 2023,Batch  3\nProbir,Focus on the Journey\nLearning C Language with Phitron\n");
     return 0; 
 }
