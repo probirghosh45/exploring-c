ccdff341f5ce73fb1c57933a860c602b0862c196
probir ghosh <probirghosh@outlook.com>
2023-04-03 10:19:27 +0600

    module 2 : if-else condition (type-2) practiced

---
+++ module-2/conditional_2.cc
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+   int rent;
+   scanf("%d",&rent);
+
+  if (rent>=1000)
+  {
+    printf("আমরা Uber এ ঘুরবো পুরো Dhaka City🤗!!!");
+  }
+  else if (rent>=300)
+  {
+    printf("OBHAI ডাকমু😎");
+  }
+
+  else{
+    printf("ওভাই,উবার Pathao😀!!!");
+  }
+
+    return 0;
+}
