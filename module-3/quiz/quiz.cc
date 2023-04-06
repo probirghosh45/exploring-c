c63b15f24c69215ab227abb3cdcaa629a51d4ad6
probir ghosh <probirghosh@outlook.com>
2023-04-06 23:43:03 +0600

    sovling module-3 quiz :tada:

--- module-3/quiz/quiz.cc
+++ module-3/quiz/quiz.cc
@@ -1,11 +1,77 @@
-#include <stdio.h>
-int main()
-{
-    int k;
-    for (k = -3; k < -5; k++)
-        printf("Hello ");
-    return 0;
-}
+//Quiz : 1
+// #include <stdio.h>
+// int main()
+// {
+//     int k;
+//     for (k = -3; k < -5; k++)
+//         printf("Hello ");
+//     return 0;
+// }
 
 // output : no output since -3 is less than -5 So after checking the condition it will not entry into the loop.
- 
+
+// Quiz : 3 [Quiz 2 , Ques. Only]
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 0;
+//     while (i < 3) {
+//         i++;
+//     }
+//    printf("%d",i);
+//    return 0;
+// }
+
+// output : 3
+
+// #include <stdio.h>
+// int main()
+// {
+//     int a = 0, i;
+//     for (i = 0;i < 5; i++)
+//     {
+//         a++;
+//         continue;
+//     }
+//     printf("%d %d",i,a);
+//     return 0;
+// }
+
+// output : 5 5
+
+//Quiz : 8
+// Which of the below loop will give the same values of i in reverse order for the loop 
+
+ #include <stdio.h>
+
+// int main()
+// {
+//     int i;
+//     for(int i=0;i<5;i++)
+//     // for(int i=5;i>0;i--)
+//     // for(int i=5;i>=0;i--)
+//     //   for(int i=5-1;i>0;i--)
+//     // for(int i=5-1;i>=0;i--)
+//     {
+//         printf("%d\n",i);
+//     }
+
+//     return 0;
+// }
+
+// Correct loop : ?
+
+//Quiz : 9
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 2;
+//     while (i = 1) {
+//         printf("Hello\n");
+//     }
+//     return 0;
+// }
+
+
+// Output : ?
