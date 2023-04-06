ac1c0da4ef38e635a66651e65f2d5b3b81d3f0af
probir ghosh <probirghosh@outlook.com>
2023-04-06 23:01:21 +0600

    sovling module-3 quiz :tada:

--- module-3/quiz/quiz.cc
+++ module-3/quiz/quiz.cc
@@ -2,7 +2,10 @@
 int main()
 {
     int k;
-    for (k = -7; k < -5; k++)
+    for (k = -3; k < -5; k++)
         printf("Hello ");
     return 0;
 }
+
+// output : no output since -3 is less than -5 So after checking the condition it will not entry into the loop.
+ 
