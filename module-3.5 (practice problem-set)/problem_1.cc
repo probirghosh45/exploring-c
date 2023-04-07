c236fbb6f530fd1e0fe18b91fcecb3d5c6f258c6
probir ghosh <probirghosh@outlook.com>
2023-04-07 09:19:33 +0600

    module - 3.5 , Problem Set Resolved :hammer:

---
+++ module-3.5 (practice problem-set)/problem_1.cc
@@ -0,0 +1,16 @@
+// hacker_ank
+https://www.hackerrank.com/contests/batch-03-a-c-programming-a-module-3-5-practice/challenges
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    printf("I Love Practice");
+    return 0;
+}
+
+
+// output : I Love Practice
---
+++ module-3.5 (practice problem-set)/problem_5.cc
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+  int i, n;
+  scanf("%d", &n);
+
+  for (i = 1; i <= n; i++)
+  {
+    if (i % 5 == 0)
+    {
+      printf("%d Yes\n", i);
+    }
+    else{
+      printf("%d No\n", i);
+    }
+
+    
+  }
+
+  return 0;
+}
