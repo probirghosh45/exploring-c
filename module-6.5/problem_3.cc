70859007229327de522ce705566aba31031363d2
probir ghosh <probirghosh@outlook.com>
2023-04-13 10:25:33 +0600

    module 6.5 problem 2 solved :tada:

---
+++ module-6.5/problem_3.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,a;
+    for(i=1;i<=a;i++){
+        scanf("%d",&a);
+        if(a%i==0){
+            printf("%d\n",i);
+        }
+    }
+    return 0;
+}
+
