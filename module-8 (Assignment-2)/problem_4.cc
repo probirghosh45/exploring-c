f16ef460599af2c5209ef6ea998c1a5e1f61558c
probir ghosh <probirghosh@outlook.com>
2023-04-13 21:25:53 +0600

    module 8  Assignment 2 (problem 4) resolved :tada:

--- module-8 (Assignment-2)/problem_4.cc
+++ module-8 (Assignment-2)/problem_4.cc
@@ -2,18 +2,17 @@
 
 int main()
 {
-    int n1,n2, m2; 
-    int arr[n1];
-
+    int n1, n2, m2;
     scanf("%d", &n1);
+    int arr[n1];  // n1 er value assign korar pore arraay modhe rakhte hobe
     for (int i = 0; i < n1; i++)
     {
         scanf("%d", &arr[i]);
     }
 
-    scanf("%d%d", &n2 ,&m2);
+    scanf("%d%d", &n2, &m2);
 
-    arr[n2]=m2;
+    arr[n2] = m2;
 
     // reverse the array
     for (int i = n1 - 1; i >= 0; i--)
