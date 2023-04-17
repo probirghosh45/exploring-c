77b326121d6370d13616f3ec7aec65a1dc14b037
probir ghosh <probirghosh@outlook.com>
2023-04-17 19:49:38 +0600

    module 10 , problem -3 : resolved :tada:

--- module-10/problem_3.cc
+++ module-10/problem_3.cc
@@ -5,15 +5,17 @@ int main()
     char a[1000001];
     gets(a);
     int count = 0;
-
+    int sum = 0;
     for (int i = 0; i < strlen(a); i++)
     {
         // printf("%d ", i);
         // printf("%c", a[i]);
-        printf("%d \n", a[i]); //output = 49 means ASCII value of 1
-        
+        // printf("%d \n", a[i]); //output = 49 means ASCII value of 1
+        // printf("%d",a[i]-48); // ASCII value of  0 is 48
+        // printf("%d",a[i]-'0');
+        sum += a[i]-'0';  
     }
 
-    // printf("%d\n",);
+    printf("%d\n",sum);
     return 0;
 }
