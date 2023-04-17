578923845fcd779187ec6e1ea74e23cb720487ed
probir ghosh <probirghosh@outlook.com>
2023-04-17 12:31:26 +0600

    module 10 , problem -1 : resolved :tada:

---
+++ module-10/problem_1.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char a[1001];
+    char b[1001];
+    int lengthOne;
+    int lengthTwo;
+
+    scanf("%s%s", &a,&b);
+    lengthOne = strlen(a);
+    lengthTwo = strlen(b);
+    printf("%d %d\n",lengthOne,lengthTwo);
+    printf("%s %s",a,b);
+    return 0;
+}
+
