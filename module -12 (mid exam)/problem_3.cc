10a5b5c82fbf83799b689e85c30f817e47393435
probir ghosh <probirghosh@outlook.com>
2023-04-21 10:22:16 +0600

    module 12 , mid exam problem_3 resolved :hammer:

--- module -12 (mid exam)/problem_3.cc
+++ module -12 (mid exam)/problem_3.cc
@@ -8,8 +8,7 @@ int main()
     if (a>=0 && a <= 100 &&  b>=0  && b <= a && c>= 0 && c <= 20)
 
     {
-        int result = (a*c + b-1)/b;
-        printf("%d", result);
+        printf("%d", (a*c)/b);
     }
     return 0;
 }
